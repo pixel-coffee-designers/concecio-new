2c6b6b6fdd90c5815b9b91945d127d605d508a47
unknown <pixelcoffeedesigners@gmail.com>
2023-01-21 22:10:33 -0300

    first commit

---
+++ carousell.js
@@ -0,0 +1,12 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide', {
+      type   : 'loop',
+      perPage: 1,
+      pagination:false,
+    } );
+    splide.mount();
+  } );
+
+
+
+  
